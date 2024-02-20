d89df4c0c4bb177d26d3de5cba34f940d3d35797
user <mail@example.com>
2024-02-19 22:06:32 -0500

    se optimiza la busqueda de archivos

--- change_wallpaper/chwall.sh
+++ change_wallpaper/chwall.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash
 
 searchPath='IMAGE_FOLDERS'
-files=($(find $searchPath -type f));
 sleep_time=300;
 
 while [ 1 ]
 do
-    index=$(($RANDOM % (${#files[@]})))
     for ln in "$(ps -eo user,args | grep "dbus-daemon.*--session.*--address=" | grep -v grep)";
     do
         user_list="$(echo "$ln" | cut -d' ' -f 1)";
@@ -16,8 +14,9 @@ do
     do
         uid=$(id -u $user);
         bus_addr=unix:path=/run/user/$uid/bus;
-        DBUS_SESSION_BUS_ADDRESS="$bus_addr" sudo -Hu $user -E /usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:///${files[$index]}";
-        DBUS_SESSION_BUS_ADDRESS="$bus_addr" sudo -Hu $user -E /usr/bin/gsettings set org.gnome.desktop.background picture-uri-dark "file:///${files[$index]}";
+        file=$(find $searchPath -type f | shuf -n 1);
+        DBUS_SESSION_BUS_ADDRESS="$bus_addr" sudo -Hu $user -E /usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:///$file";
+        DBUS_SESSION_BUS_ADDRESS="$bus_addr" sudo -Hu $user -E /usr/bin/gsettings set org.gnome.desktop.background picture-uri-dark "file:///$file";
     done 
     sleep $sleep_time
 done
