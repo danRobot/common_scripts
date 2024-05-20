0a0d7892af1d37af3bdcffebf5e0f443c3b1046b
user <mail@example.com>
2024-05-19 22:56:43 -0500

    optimización

--- automatic_umount_disks/shtdwn_custom_script.py
+++ automatic_umount_disks/shtdwn_custom_script.py
@@ -2,52 +2,71 @@
 
 from os import popen
 import subprocess
+from multiprocessing import Pool
+import json
 from time import sleep
 
 def kill_busy(umount_device):
     result= subprocess.run(['fuser','-mvk',umount_device], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    print('ERROR',result.stderr)
-    print('RESULT',result.stdout)
-    sleep(1)
-
-
-rs=popen('lsblk')
-lines=rs.read().split('\n')
-letras=['a','b','c','d','e','f','j']
-for letra in letras:
-    disco='sd'+letra
-    for i,e in enumerate(lines):
-        if disco in e:
-            c=i+1
-            break
-        else:
-            c=-1
-    result = subprocess.run(['udisksctl','power-off','-b','/dev/'+disco], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     #print('ERROR',result.stderr)
     #print('RESULT',result.stdout)
-    if c!=-1:
-        if lines[c][0]=='└':
-            device=lines[c][2:].split(' ')[0]
-            if 'luks' in device:
-                umount_device='/dev/mapper/'+device
+    sleep(0.5)
+
+def get_disks(device):
+    try:
+        lists=device['children']
+        result=map(get_disks,lists)
+        return {'device_name':device['name'],'tran':device['tran'],'parts':list(result)}
+    except Exception as e:
+        try:
+            if device['type']=='crypt':
+                umount_device='/dev/mapper/'+device['name']
             else:
-                umount_device='/dev/'+device
-            kill_busy(umount_device)
-            result = subprocess.run(['umount','-f',umount_device], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            print('ERROR',result.stderr)
-            print('RESULT',result.stdout)
-            if 'luks' in device:
-                try:
-                    result = subprocess.run(['cryptsetup','close',device], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                except Exception as e:
-                    print(e)
-                    pass
-                print('ERROR',result.stderr)
-                print('RESULT',result.stdout)
-            result = subprocess.run(['udisksctl','power-off','-b','/dev/'+disco], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            print('ERROR',result.stderr)
-            print('RESULT',result.stdout)
-            if len(result.stderr)>0:
-                print('fallo')
-rs.close()
+                umount_device='/dev/'+device['name']
+        except:
+            ##print('falla',device)
+            return device
+        return {'device_name':device['name'],'type':device['type'],'tran':device['tran'],'parts':umount_device}
+
+def umount(list_disks):
+    if type(list_disks['parts'])==type([]):
+        e=map(umount,list_disks['parts'])
+        return list(e)
+    if type(list_disks['parts'])==type(''):
+        kill_busy(list_disks['parts'])
+        result = subprocess.run(['umount','-f',list_disks['parts']], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        #print('ERROR umount',result.stderr)
+        #print('RESULT',result.stdout)
+        if list_disks['type']=='crypt':
+            try:
+                result = subprocess.run(['cryptsetup','close',list_disks['device_name']], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+                #print('ERROR close',result.stderr)
+                #print('RESULT',result.stdout)
+            except:
+                pass
+            return 'close '+list_disks['parts']
+        pass
+
+
+def safe_umount(list_disks):
+    msg,e='',''
+    if list_disks['tran']=='usb':
+        if type(list_disks['parts'])==type([]):
+            e=list(map(umount,list_disks['parts']))
+            msg=''
+        result = subprocess.run(['udisksctl','power-off','-b','/dev/'+list_disks['device_name']], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        #print('ERROR',result.stderr)
+        #print('RESULT',result.stdout)
+    return (msg,e)
+
+string=popen("lsblk  -J -o 'NAME,TYPE,TRAN,TYPE,FSTYPE,MOUNTPOINT'").read()
+list_json=json.loads(string)
+blockdevices=list_json['blockdevices']
+
+list_disks = list(map(get_disks,blockdevices))
+
+with Pool(len(list_disks)) as pool:
+    result=pool.map_async(safe_umount,list_disks)
+    print(result.get())
+
 exit()
